19f37cc3c8233b205dfe9249b28f8737727954e1
Fernando Palacios <deliopalaciosviafara@gmail.com>
2025-11-05 04:08:28 -0500

    refactor types to use specific language types for state and actions

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,22 +1,27 @@
 // Definición de tipos
 import { AUTO_LANGUAGE, type SUPORTED_LANGUAGES } from './constants'
 
-export type Language = keyof typeof SUPORTED_LANGUAGES
 export type AutoLanguage = typeof AUTO_LANGUAGE
 export type FromLanguage = Language | AutoLanguage
+export type Language = keyof typeof SUPORTED_LANGUAGES
 
 // Para definir el contrato de un objeto es más recomendable usar una interface
 export interface State {
-  fromLanguage: string,
-  toLanguage: string,
+  fromLanguage: FromLanguage,
+  toLanguage: Language,
   fromText: string,
   result: string,
   loading: boolean
 }
 
 export type Action =
-   | { type: 'SET_FROM_LANGUAGE', payload: string }
-   | { type: 'SET_TO_LANGUAGE', payload: string }
+   | { type: 'SET_FROM_LANGUAGE', payload: FromLanguage }
+   | { type: 'SET_TO_LANGUAGE', payload: Language }
    | { type: 'SET_FROM_TEXT', payload: string }
    | { type: 'SET_RESULT', payload: string }
-   | { type: 'SWAP_LANGUAGES', }
+   | { type: 'SWAP_LANGUAGES' }
+
+export enum SectionType {
+  FROM = 'from',
+  TO = 'to'
+}
